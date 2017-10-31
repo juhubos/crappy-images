d8479caab3e8ef02640c8d53c02f3443e9c0ce41
jhulsebos <juhubos@gmail.com>
2017-10-31 12:59:24 -0700

    canvas draw functional, starting bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express"),
-    app = express();
+    app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/crappy_images");
+app.use(express.static('public'));  //allows you to serve static files (app.css)
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -11,11 +12,17 @@ app.get("/", function(req, res) {
     res.redirect("/images");
 });
 
+//INDEX
 app.get("/images", function(req, res) {
-    res.send("IT worked!");
+    res.render("index");
 });
+
+//NEW
+// app.get("/images/new", function(req, res) {
+
+// })
 
 
 app.listen(3000, function() {
-    console.log("Crappy Image Is Running!")
+    console.log("Crappy Image Server Is Running!")
 })
